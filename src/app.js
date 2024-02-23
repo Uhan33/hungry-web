2d4abf11b28b5275edd1b1d4c9eb4f85cea65c72
Uhan33 <jazzcafe12@kangwon.ac.kr>
2024-02-23 18:32:14 +0900

    add: 초기 파일 세팅
    기본적으로 사용하는 코드 및 라이브러리 추가하였습니다.

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import router from './routes/index.js';
+import LogMiddleware from './middlewares/log.middleware.js';
+import ErrorHandlingMiddleware from './middlewares/error-handling.middleware.js';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+app.use(LogMiddleware);
+app.use(express.json());
+app.use(cookieParser());
+app.use('/api', router);
+app.use(ErrorHandlingMiddleware);
+
+
+app.listen(port, () => {
+  console.log(port, '포트로 서버가 열렸어요!');
+});
