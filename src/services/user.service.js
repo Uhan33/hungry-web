2dd214dd7a44d2b056d767a89105b2784092d53e
👻 choi <choi@ui-noteubug.local>
2024-02-26 10:43:48 +0900

    test:User Controller  Unit Test

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -4,11 +4,11 @@ export class UsersService{
     constructor(usersReposity){
         this.usersReposity = usersReposity
     }
-    createUser = async(email, name, password, confirmPassword, addr, number, role)=>{
+    createUser = async(email, name, password, addr, number, role)=>{
         const user = await this.usersReposity.findByUserEmail(email)
         if (user) throw new Error ('이미 존재하는 email입니다.')
         const hashedPassword = await bcrypt.hash(password, 10)
-        const createdUser = await this.usersReposity.createUser(email, name, hashedPassword, confirmPassword, addr, number, role)
+        const createdUser = await this.usersReposity.createUser(email, name, hashedPassword, addr, number, role)
         return {
             email : createdUser.email,
             name : createdUser.name,
