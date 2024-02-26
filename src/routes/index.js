48ebf12827c769790798a5f0904cbea30564261c
oooo91 <hyelneu29@gmail.com>
2024-02-26 12:15:59 +0900

    modify : schema -> salesAmount컬럼 추가, index.js -> router 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import OrdersRouter from './orders.router.js'
+import ReviewRouter from './review.router.js';
+import MainRouter from './main.router.js';
 
 const router = express.Router();
 
+router.use('/review/', ReviewRouter);
+router.use('/store/', MainRouter);
 router.use('/orders/', OrdersRouter);
 
 export default router;
