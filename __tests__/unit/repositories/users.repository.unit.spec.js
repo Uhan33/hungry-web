eb8efaa7c4fe31391fd76fb4ab50462fde4ed918
👻 choi <choi@ui-noteubug.local>
2024-02-26 12:47:09 +0900

    test: User Repository, User Create Success

--- __tests__/unit/repositories/users.repository.unit.spec.js
+++ __tests__/unit/repositories/users.repository.unit.spec.js
@@ -1,7 +1,6 @@
 import { jest } from '@jest/globals';
 import { UsersRepository } from '../../../src/repositories/user.repositiry.js';
-import { describe } from '@jest/globals';
-import { beforeEach } from '@jest/globals';
+import { expect } from '@jest/globals';
 
 let mockPrisma = {
   users: {
@@ -18,42 +17,18 @@ describe('Users Unit Test', () => {
 
   describe('유저 생성', () => {
     test('유저 생성 성공', async () => {
-        const createdUser ={
-            eamil
-        }
-        mockPrisma.users.create.mockReturnValue(mockReturn);
-    
-        // createUserParams Method를 실행하기 위해 필요한 Params 입니다.
-        const createUserParams = {
-          email: 'test@test.com',
-          name: '최하온',
-          password: 'hashpassword',
-          confirmPassword: 'hashpassword',
-          addr: '청주시',
-          number: '010-1234-5678',
-          role: 'onwer',
-          point: {
-            create: {
-                money: 1000000
-            }
-        }
-        };
-    
-        // usersRepository의 createPost Method를 실행합니다.
-        const createUserData = await usersRepository.createUser(
-            createUserParams.email,
-            createUserParams.name,
-            createUserParams.password,
-            createUserParams.confirmPassword,
-            createUserParams.addr,
-            createUserParams.number,
-            createUserParams.role,
-        );    
-        // createPostData는 prisma.posts의 create를 실행한 결과값을 바로 반환한 값인지 테스트합니다.
-        expect(createUserData).toBe(mockReturn);
-        expect(mockPrisma.users.create).toHaveBeenCalledWith({
-            data: createUserParams,
-          });
+      const createdUser = {
+        email: 'test@test.com',
+        name: '홍길동',
+        password: '123456',
+        addr: '청주시',
+        number: '010-1234-5678',
+        role: 'owner',
+        point: 1,
+      };
+      mockPrisma.users.create.mockReturnValue(createdUser)
+      const create = await usersRepository.createUser(createdUser) 
+      expect(create).toEqual(createdUser)
     });
   });
 });
